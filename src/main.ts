0fa4dccd930c1511c9864c5a18441d3d7b60611a
idarciooliveira <idarciooliveira@gmail.com>
2024-04-02 14:45:28 +0100

    update packages

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import path from 'path';
 import * as fs from 'fs'
 import { db } from './db';
-import { Schema } from './type';
+import { Schema as DatasetSchema } from './type';
 import * as PgSchema from '../drizzle/schema'
 import JSONStream from 'big-json'
 
@@ -12,10 +12,12 @@ const readStream = fs.createReadStream(file, 'utf-8')
 
 const jsonStream = JSONStream.createParseStream()
 
+var count = 5000;
 
-jsonStream.on('data', (data: Schema[]) => {
+jsonStream.on('data', (data: DatasetSchema[]) => {
     try {
-        var count = 5500;
+        console.log('Initial stream!')
+
         data.forEach(element => {
             count++
             db.insert(PgSchema.places)
@@ -37,16 +39,16 @@ jsonStream.on('data', (data: Schema[]) => {
                     isApproved: true,
                 })
                 .onConflictDoNothing()
-                .then()
+                .then(res => { })
         });
 
-        console.log(count)
+        console.log('...')
     } catch (error) {
         console.error('Error parsing JSON:', error);
     }
 })
 
-jsonStream.on('end', () => console.log('finished task'))
+jsonStream.on('end', () => console.log('finished'))
 
 //@ts-ignore
 readStream.pipe(jsonStream)
